a9f6dfb96871a89d87af0c634212443f9bf90344
Jiya Patidar <jiyapatidarkasrawad@gmail.com>
2024-02-19 22:04:39 +0530

    Create q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std ;
+
+int pivot(int arr[],int n){
+    int s =0;
+    int e = n-1 ;
+    int mid = s+(e-s)/2 ;
+
+    
+    while(s<e){
+if(arr[mid]>= arr[0]){
+
+    s=mid +1 ;
+
+}
+else {
+    e =mid ;
+
+}
+mid =s+(e-s)/2 ;
+
+    }
+    return s ;
+
+}
+
+int main(){
+
+    int arr[5]={1,8,10,17,3};
+    cout<<pivot(arr,5);
+    return 0 ;
+
+}
